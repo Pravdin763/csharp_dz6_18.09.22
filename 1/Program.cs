4e06afad03436a4468d056ae6b7a376c07d06f09
igor <igor.pravdin163@gmail.com>
2022-09-18 12:07:07 +0400

    добавил 1

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите колличество чисел: ");
+int m = int.Parse(Console.ReadLine()!);
+
+
+int CheckFor0(int m1)
+{
+    int count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите число {i + 1} из {m1}");
+        float input = float.Parse(Console.ReadLine()!);
+        if (input > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+void Print()
+{
+    Console.WriteLine($"Колличество чисел больше 0 введенных пользователем  = {CheckFor0(m)}");
+}
+
+Print();
